b41dff770314a22c6961d1b27fb10cf5ed2929e0
lkwWalter <39405921+lkwWalter@users.noreply.github.com>
2024-08-13 17:12:50 +0200

    mod : prompter

--- components/prompter/test.js
+++ components/prompter/test.js
@@ -1,33 +1,3 @@
-import { ComfyUIApiClient, ComfyUIWorkflow } from "@stable-canvas/comfyui-client";
+import * as prompter from "./prompter.js";
 
-const workflow = new ComfyUIWorkflow();
-
-// connect ws client
-client.connect();
-const cls = workflow.classes;
-const [model, clip, vae] = cls.CheckpointLoaderSimple({
-    ckpt_name: "lofi_v5.baked.fp16.safetensors",
-});
-const enc = (text) => cls.CLIPTextEncode({ text, clip })[0];
-const [samples] = cls.KSampler({
-    seed: Math.floor(Math.random() * 2 ** 32),
-    steps: 35,
-    cfg: 4,
-    sampler_name: "dpmpp_2m_sde_gpu",
-    scheduler: "karras",
-    denoise: 1,
-    model,
-    positive: enc("best quality, 1girl"),
-    negative: enc(
-        "worst quality, bad anatomy, embedding:NG_DeepNegative_V1_75T"
-    ),
-    latent_image: cls.EmptyLatentImage({
-        width: 512,
-        height: 512,
-        batch_size: 1,
-    })[0],
-});
-cls.SaveImage({
-    filename_prefix: "from-sc-comfy-ui-client",
-    images: cls.VAEDecode({ samples, vae })[0],
-});
+prompter.main("1723123845.mp4", "destiny, gadget, underwater, lovestory, animals, photrealistic, computer ");
