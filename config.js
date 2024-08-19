f3755d318c09251b1a7a214ac2e952327e26df66
lkwWalter <39405921+lkwWalter@users.noreply.github.com>
2024-08-19 20:44:30 +0200

    CleanUp: .bak old comfy workflows

--- config.js
+++ config.js
@@ -1,18 +1,39 @@
-import * as workflower from "./workflow/workflow_api.json"
-export let comfyUiServerAddresse  ="127.0.0.1:8188/";
-export let inbox = "./inbox"
-export let outbox = "./outbox"
+// config.js
+// ComfyUi Configuration
+import * as workflower from "./workflow/workflow_api.json";
+export let workflow = workflower;
+export let comfyUiServerAddresse = "127.0.0.1:8188/";
 
 export let promptText = {
-    "happy": "  ",
-    "sad": "  ",
-    "angry": "  ",
-    "fearful": "  ",
-    "disgusted": "  ",
-    "surprised": "  ",
-    "neutral": "  "
-}
-export let workflow = workflower
+  happy: "  ",
+  sad: "  ",
+  angry: "  ",
+  fearful: "  ",
+  disgusted: "  ",
+  surprised: "  ",
+  neutral: "  ",
+};
 
-console.log(workflow)
-console.log(promptText.happy) 
+//Folder Paths
+export let folderPath = "C:/Users/nm/Desktop/Museumsnacht/filewatcher";
+export let inbox = folderPath + "/inbox";
+export let outbox = folderPath + "/outbox";
+
+/**
+ * This function logs all variables in the config file.
+ * @function
+ * @name logVariables
+ * @returns {undefined}
+ */
+export function logVariables() {
+  console.log("Variables in config.js:");
+  console.log("comfyUiServerAddresse:", comfyUiServerAddresse);
+  console.log("inbox:", inbox);
+  console.log("outbox:", outbox);
+  console.log("promptText:", promptText);
+  console.log("workflow:", workflow);
+
+  console.log("End of config.js");
+}
+
+logVariables();
